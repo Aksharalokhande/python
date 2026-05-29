de12198413b779eec6cf01446eab458244b50d1b
Akshara Lokhande <aksharalokhande5@gmail.com>
2026-05-29 20:50:40 +0530

    first commit

---
+++ marks.py
@@ -0,0 +1,22 @@
+sub1=int(input(" Enter subject1 marks: "))
+sub2=int(input(" Enter subject2 marks: "))
+sub3=int(input(" Enter subject3 marks: "))
+sub4=int(input(" Enter subject4 marks: "))
+sub5=int(input(" Enter subject5 marks: "))
+
+total=sub1+sub2+sub3+sub4+sub5
+percentage=total/5
+
+print("Total marks=",total)
+print("Percentage=",percentage)
+
+if percentage>=75:
+    print(" Result:Distinction")
+elif percentage>=60:
+    print("Result:first class")   
+elif percentage>=45:
+    print("Result:pass")   
+else:
+    print("Result:fail")    
+     
+
