7f802bb2e8665b9932fc7f402329fa96eb99481a
Akshara Lokhande <aksharalokhande5@gmail.com>
2026-06-28 21:56:37 +0530

    added contact

--- app2.py
+++ app2.py
@@ -46,6 +46,11 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+    
     
 
 
